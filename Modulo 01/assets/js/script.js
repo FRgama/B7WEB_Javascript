ee479de93b35e2f6e64635ed15d1cdd27434839e
Rodrigo Gama <121040163+FRgama@users.noreply.github.com>
2025-06-03 16:56:25 -0300

    Módulo 01 adicionado

---
+++ Modulo 01/assets/js/script.js
@@ -0,0 +1,102 @@
+//alert("Olá Mundo!")
+
+//console.log("Olá Mundo 2.0")
+
+/*
+let nome = "Gama"
+let idade = 90
+let estado = "São Paulo"
+let cidade = "Embu das Artes"
+
+console.log(nome)
+console.log(idade)
+console.log(estado)
+console.log(cidade)
+*/
+
+/*
+let lista = ["ovo", "farinha", "leite"]
+let nomeCompleto = {nome: "Rodrigo", idade: 20}*/
+
+/*let nome = "Rodrigo";
+let sobrenome = "Gama";
+let idade = 10
+let nomeCompleto = `Nome: ${nome} ${sobrenome}`
+let idadeString = `Idade: ${idade} anos`
+console.log(nomeCompleto + " " + idadeString)
+*/
+
+/*
+let idade = 90
+
+if (idade >= 18) {
+    console.log('Voce é maior de idade')
+}
+else{
+    console.log('Voce é menor de idade')
+}
+*/
+
+// == -> true quando o valor é igual (independente do tipo de variavel)
+// === -> quando o valor é igual (inclusive o tipo da variavel)
+
+// && na condicional -> funciona como um "AND"
+
+
+/*
+// exercicio de condicionais
+
+// substitua o @ pela condicional correta para que o resultado seja true
+
+//x = 10, y = 5 console.log(x @ y)
+
+let x = 10;
+let y = 5;
+console.log(x>y);
+
+//substitua o @ pela condicional correta pare que o valor seja false
+// w = "10" z = 10 console.log(w @ z)
+
+let w = "10"
+let z = 10
+console.log(w === z)
+
+//crie uma condicional para verificar se o valor da carne esta barato ou caro
+// até 45 é barato
+
+let preco = 40.3
+
+if(preco <= 45){
+    console.log("a carne esta barata")
+}
+else{
+    console.log("a carne está cara")
+}
+*/
+
+//condicional ternaria
+
+/*
+let isMember = true;
+let shipping = isMember ? 2 : 10;
+console.log(isMember ? "voce é membro" : "voce nao é membro")
+console.log("Frete: " + shipping)
+*/
+
+/*
+//Switch
+let profession = 'Soldado';
+switch (profession) {
+    case 'Soldado':
+        console.log("Camisa Verde");
+        break;
+    case 'bombeiro':
+        console.log("Camisa Vermelha")
+        break;
+    case 'Policial':
+        console.log("Camisa Azul")
+    default:
+        console.log("Camisa Branca")
+        break;
+}
+*/
